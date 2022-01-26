823f83ed55aecad0efb003c84751efbbe65ba78d
Hari Om <58305594+Madhav008@users.noreply.github.com>
2022-01-26 22:23:07 +0530

    Done Changes

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ require("dotenv").config();
 const app = express();
 
 const url = "https://inshorts.com/en/read/";
-app.get("/Q4WXV7CoRNDkcVse52FT", async function (req, res) {
+app.get("/", async function (req, res) {
 
    result =await scrapper.main(url);
 
     await res.send(result);
 });
-app.get("/Q4WXV7CoRNDkcVse52FT/:title", async function (req, res) {
+app.get("/:title", async function (req, res) {
 
   result =await scrapper.main(url+req.params.title);
 
